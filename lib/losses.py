Fix localization loss calculation

lib/losses.py
@@ -9,6 +9,8 @@ implemented here.
 import tensorflow as tf
 from tensorflow.keras.losses import Loss, Reduction
 
+from lib.np_bbox_utils import boxes_classes
+
 __author__ = 'Helmuth Breitenfellner'
 __copyright__ = 'Copyright 2021, Christian Doppler Laboratory for ' \
                 'Embedded Machine Learning'
@@ -75,21 +77,29 @@ class SSDLosses(Loss):
         return config
 
     def call(self, labels, logits):
+        """"Calculate the losses from the predicted logits.
+        The structure of the data (logits and labels) consists of four
+        (predicted or real) adjustments (data[:, :, :4]),
+        and N (number of classes) logits.
+        """
+        # split into location & class-predictions
+        labels_box, labels_cls = boxes_classes(labels)
+        logits_box, logits_cls = boxes_classes(logits)
         # classification loss: using softmax_cross_entropy_with_logits
         cls_loss = tf.nn.softmax_cross_entropy_with_logits(
-            labels=labels[:, :, 4:],
-            logits=logits[:, :, 4:],
+            labels=labels_cls,
+            logits=logits_cls,
         )
         # calculate softmax of logits
-        y_pred = tf.nn.softmax(logits)
+        # y_pred_cls = tf.nn.softmax(logits_cls)
         # classification loss: using softmax loss
         # cls_loss = softmax_loss(labels[:, :, :-4], y_pred[:, :, :-4])
         # localization loss: using smooth L1 loss
-        loc_loss = smooth_l1(labels[:, :, :4], y_pred[:, :, :4])
+        loc_loss = smooth_l1(labels_box, logits_box)
         # which true items are negative (i.e. "background" class)?
-        y_neg = labels[:, :, 4]
+        y_neg = labels_cls[:, :, 0]
         # which true items are positive (i.e. a class != background)?
-        y_pos = tf.reduce_sum(labels[:, :, 5:], axis=-1)
+        y_pos = tf.reduce_sum(labels_cls[:, :, 1:], axis=-1)
         # number of positives & negatives in the batch
         n_pos = tf.reduce_sum(y_pos)
         n_neg = tf.cast(tf.math.count_nonzero(y_neg*cls_loss), tf.float32)
@@ -128,70 +138,3 @@ class SSDLosses(Loss):
             "combined": (neg_cls_loss + pos_cls_loss + pos_loc_loss) * f
         }
         return losses[self.key]
-
-
-class SSDLoss(Loss):
-    """SSD Loss for a training or validation batch.
-    It takes the ground truth (class predictions and box adjustments) and
-    the predictions for multiple boxes per instance.
-    """
-    def __init__(self, num_classes, reduction=Reduction.AUTO,
-                 name='ssd_loss'):
-        super(SSDLoss, self).__init__(reduction=reduction, name=name)
-        self.num_classes = num_classes
-
-    def get_config(self):
-        config = super(SSDLoss, self).get_config()
-        config['num_classes'] = self.num_classes
-        return config
-
-    def call(self, labels, logits):
-        # classification loss: using softmax_cross_entropy_with_logits
-        if True:
-            cls_loss = tf.nn.softmax_cross_entropy_with_logits(
-                labels=labels[:, :, 4:],
-                logits=logits[:, :, 4:],
-            )
-        # calculate softmax of logits
-        y_pred = tf.nn.softmax(logits)
-        # classification loss: using softmax loss
-        # cls_loss = softmax_loss(labels[:, :, :-4], y_pred[:, :, :-4])
-        # localization loss: using smooth L1 loss
-        loc_loss = smooth_l1(labels[:, :, :4], y_pred[:, :, :4])
-        # which true items are negative (i.e. "background" class)?
-        y_neg = labels[:, :, 4]
-        # which true items are positive (i.e. a class != background)?
-        y_pos = tf.reduce_sum(labels[:, :, 5:], axis=-1)
-        # number of positives & negatives in the batch
-        n_pos = tf.reduce_sum(y_pos)
-        n_neg = tf.cast(tf.math.count_nonzero(y_neg*cls_loss), tf.float32)
-        # how many negatives to "hard mine"?
-        n_neg_hard = tf.cast(
-            tf.minimum(n_neg, tf.maximum(3*n_pos, 3)),
-            tf.int32)
-
-        def neg_loss():
-            # we now calculate the class prediction loss for negatives
-            # and pick the top n_neg_hard in the batch
-            loss_lin = tf.reshape(cls_loss * y_neg, [-1])
-            vals, idxs = tf.nn.top_k(loss_lin, k=n_neg_hard)
-            # we get the last value and use it to filter
-            min_loss = vals[-1]
-            mask = tf.logical_and(tf.cast(y_neg, tf.bool), cls_loss > min_loss)
-            mask = tf.cast(mask, tf.float32)
-            # for the negative loss there is no location loss so that's all
-            return tf.reduce_sum(cls_loss * mask, axis=-1)
-
-        def zero_loss():
-            return tf.zeros([tf.shape(labels)[0]])
-
-        neg_cls_loss = tf.cond(tf.equal(n_neg, 0), zero_loss, neg_loss)
-        # now the positive loss sum - location and classification
-        pos_loc_loss = tf.reduce_sum(loc_loss * y_pos, axis=-1)
-        pos_cls_loss = tf.reduce_sum(cls_loss * y_pos, axis=-1)
-        # scaling factor: relative to the number of positive boxes,
-        # and multiplied by batch size
-        f = tf.cast(tf.shape(labels)[0], tf.float32) / tf.maximum(1., n_pos)
-        # we return the total loss
-        loss = (neg_cls_loss + pos_loc_loss + pos_cls_loss)
-        return loss * f